a2bf7991134ee94aca3a818b38217f80a9145c0d
Prashantijamgondi <ijprashant6@gmail.com>
2026-06-28 22:08:37 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: 'all',
+    allowedHosts: 'https://user-management-dashboard-3zta.onrender.com',
   },
 })
